aa5f910c5f010d0ec81df72d3188ae9e429a1991
hurtonypeter <hurtonypeter@gmail.com>
2015-06-29 12:28:51 +0200

    init

---
+++ EnCoOrszag/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnCoOrszag.Startup))]
+namespace EnCoOrszag
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
