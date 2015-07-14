e4bc4098464b0a8c257d25c8d0d4ee8cfdc8de33
freamcoding@gmail.com <freamcoding@gmail.com>
2015-07-14 16:23:22 +0200

    Started Angular, Json converting to do

---
+++ EnCoOrszag/Scripts/GameScripts/Assault.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var app = angular.module('AssaultApp', []).v;
+
+    
+
+    app.controller('ArmyStoreController', function () {
+        this.loadArmy = function (army) {
+            this.army = army;
+        }
+    });
+
+
+    app.controller('TestController', function () {
+        this.setData = function (val) {
+            this.dmg = val;
+        }
+        this.minus = function (val) {
+            this.dmg -= val;
+        }
+    });
+
+})();
